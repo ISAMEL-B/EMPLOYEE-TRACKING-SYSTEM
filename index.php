2f80c9b05654bf4a5b42afb8874a1924ecfb2914
ISAMEL-B <2021bse043@std.must.ac.ug>
2025-04-12 17:56:47 +0300

    update

--- index.php
+++ index.php
@@ -8,8 +8,11 @@ include 'Dashboard/main/approve/config.php'; // Your database connection file
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MUST HRM Scorecard System</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"> -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="Dashboard/components/src/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="Dashboard/components/bootstrap/css/bootstrap.min.css">
+
     <style>
         :root {
             --must-primary: #003366;
